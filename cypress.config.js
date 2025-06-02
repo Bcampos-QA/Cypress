5bab7212986d52b399acc4cc4920392f4bc3417d
Bruno Guerreiro <brunoguerreiro.campos@hotmail.com>
2025-06-02 18:59:30 -0300

    Configure the ci workflow

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,10 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {},
+  e2e: {// Altere o specPattern para incluir sua pasta 'integration'
+    specPattern: 'cypress/integration/**/*.{js,jsx,ts,tsx}', // Ou apenas '*.js', '*.jsx', etc.
+    // Se você tiver um baseUrl, configure-o aqui
+    // baseUrl: 'http://localhost:8080',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here},
 })
