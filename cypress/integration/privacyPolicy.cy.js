3dfdb13c5aafdf0cf71afaf7d4b2ef286475beea
Bruno Guerreiro <brunoguerreiro.campos@hotmail.com>
2025-06-02 19:20:35 -0300

    Configure the ci workflow

--- cypress/integration/privacyPolicy.cy.js
+++ cypress/integration/privacyPolicy.cy.js
@@ -3,7 +3,7 @@
 it('Testa a página da politica de privacidade de forma independente',()=>{
     cy.visit('./src/privacy.html')
 
-    cy.contains('h1','CAC TAT - Política de privacidade').should('be.visible')
-
+    cy.get('h1','CAC TAT - Política de Privacidade').should('be.visible')
+    
     cy.contains('p','Talking About Testing').should('be.visible')
 })
